c989dd9e5f188747d039356e7780c2e305c4e141
Otto <heinz-otto.klas@gmx.de>
2019-04-05 11:59:38 +0200

    Create Script1.sh

---
+++ Script1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Variables
+qpath="/media/m"
+dpath="/media/ds1"
+sdir=$(dirname $(realpath $0))
+# mount, sync and trigger
+mount $qpath
+mount $dpath
+rsync -a --delete "${qpath}/Sicherung/" "${dpath}/Sicherung/"
+rsync -a --delete "${qpath}/Scripts/" "${dpath}/Scripts/"
+bash "${sdir}/fhemcl.sh" 8083 "set Sicherung SyncEnde" > /dev/null 2>&1
