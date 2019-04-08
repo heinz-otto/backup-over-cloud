0f9e99eb4bb1aba6d1abf468dbd772f40c6dc9bd
Otto <heinz-otto.klas@gmx.de>
2019-04-08 12:58:08 +0200

    Update RegisterTaskMoveToArchive.ps1
    Add some description (Help) and bugfixing

--- RegisterTaskMoveToArchive.ps1
+++ RegisterTaskMoveToArchive.ps1
@@ -1,8 +1,23 @@
+<#
+.SYNOPSIS
+    This Script register a Task in Taskscheduler Windows
+.DESCRIPTION
+    The Task will registered with User Account logged on, without save Password
+.EXAMPLE
+    no Arguments
+.NOTES
+    it's important to capsulate the Script in argument with ""
+    The Logontype is independend from user Logon (S4U)
+    Password is not needed and not saved or asked
+    There are two triggers
+#>
 $triggers = @()
 $triggers += New-ScheduledTaskTrigger -Daily -At 08:00 
 $triggers += New-ScheduledTaskTrigger -AtStartup
 
-$action = New-ScheduledTaskAction -WorkingDirectory c:\Tools\Scripts -Execute powershell.exe -Argument "-noninteractive -command '&{C:\Tools\Scripts\FolderSyncAblauf.ps1}'"
-$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType S4U -RunLevel Highest
+$action = New-ScheduledTaskAction -WorkingDirectory c:\Tools\Scripts -Execute powershell.exe -Argument '-noninteractive -command "&{C:\Tools\Scripts\FolderSyncAblauf.ps1}"'
+$principal = New-ScheduledTaskPrincipal -UserID "$env:USERDOMAIN\$env:USERNAME" -LogonType S4U -RunLevel Limited
 
 Register-ScheduledTask -TaskName MoveCloudToArchiv –Action $action –Trigger $triggers –Principal $principal
+
+# Unregister-ScheduledTask -TaskName MoveCloudToArchiv
