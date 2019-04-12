b121275fefb686193121501a546954d4bdf5f014
Otto <heinz-otto.klas@gmx.de>
2019-04-12 22:07:35 +0200

    Update ScriptQ1.sh
    Delete Sourcefiles added

--- ScriptQ1.sh
+++ ScriptQ1.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # needs fhemcl.sh in the same path
-# Variables, fill in the right foldernames
-qpath="/media/ds1"
-dpath="/media/m"
+# Variables
+qpath="/media/zadonixs1"
+dpath="/media/magenta"
 sdir=$(dirname $(realpath "$0"))
 # mount, sync and trigger
 mount $qpath
 mount $dpath
-rsync -rutv --inplace ${qpath}/S* ${dpath}
-# cp -pRu "${qpath}/S*" "${dpath}"
-# bash "${sdir}/fhemcl.sh" 8083 "set Sicherung SyncEnde" > /dev/null 2>&1
+rsync -rut --inplace ${qpath}/S* ${dpath}
+# delete the sourcefiles
+rm -r ${qpath}/S*
+bash "${sdir}/fhemcl.sh" 8083 "set Sicherung CopyMagentaEnde" > /dev/null 2>&1
 umount $qpath
 umount $dpath
