593045b2c691feb15fac53c5c6215ad588dfef4d
Otto <heinz-otto.klas@gmx.de>
2019-04-13 22:53:29 +0200

    Update CopyMagenta.ps1
    bugfixes
    build some verbose messages

--- CopyMagenta.ps1
+++ CopyMagenta.ps1
@@ -17,15 +17,13 @@ param(
 [string]$destination
 )
 #endregion 
-Write-Verbose $sourcepath
-Write-Verbose $destination
 Set-Location $PSScriptRoot
-
-#exit
+$verbose = $PSCmdlet.MyInvocation.BoundParameters["Verbose"] 
+Write-Verbose "$sourcepath $destination"
 
 $FileHash = "LeftSideHash$(get-date -format "yyyyMMdd-HHmmss").txt"
 
-if ($fhemurl -ne "leer") {.\fhemcl.ps1 $fhemurl "set Sicherung Copy_2_$destination"}
+if (!$verbose) {.\fhemcl.ps1 $fhemurl "set Sicherung Copy_2_$destination"}
 Write-Verbose "Copy 2 $destination"
 $path=$destination + "\Scripts"
 If (-not (test-path $path)) {New-Item -Path $path -ItemType Directory| Out-Null} 
@@ -37,7 +35,7 @@ If (-not (test-path $path)) {New-Item -Path $path -ItemType Directory| Out-Null}
 $sourcefiles = gci $sourcepath -File -Recurse |where {(get-date $_.CreationTime -f "dd/MM/yy") -eq (get-date -f "dd/MM/yy")}
 $LeftSideHash = $sourcefiles | Get-FileHash | select @{Label="Path";Expression={$_.Path.Replace($sourcepath,"")}},Hash 
 $LeftSideHash |Export-Clixml $destination\Scripts\$FileHash
-if ($fhemurl -ne "leer") {.\fhemcl.ps1 $fhemurl "set Sicherung Hashfile_erzeugt"}
+if (!$verbose) {.\fhemcl.ps1 $fhemurl "set Sicherung Hashfile_erzeugt"}
 Write-Verbose "Hashfile_erzeugt"
 
 # Dateien kopieren, da einzelne Files kopiert werden ist das mit den Pfaden etwas aufwendiger
@@ -47,7 +45,7 @@ foreach ($file in $sourcefiles)
     If (-not (test-path $newdir)) { md $newdir| Out-Null}
     Copy-Item -Path $file.FullName -Destination $newdir
     $cmd1 = $("set Sicherung " + $file.fullname)
-    if ($fhemurl -ne "leer") {.\fhemcl.ps1 $fhemurl $cmd1}
+    if (!$verbose) {.\fhemcl.ps1 $fhemurl $cmd1}
     Write-Verbose $("copy " + $file.fullname)
 }
 
@@ -58,6 +56,6 @@ gci $destination\Sicherung -r | Export-Clixml $destination\Scripts\Sicherung.xml
 $SicherungStat = gci $destination\Sicherung -r | measure-object -Property length -sum
 $cmd1 = $("setreading Sicherung lastTransferCount " + $SicherungStat.count)
 $cmd2 = $("setreading Sicherung lastTransferSizeB " + $SicherungStat.sum)
-if ($fhemurl -ne "leer") {.\fhemcl.ps1 $fhemurl $cmd1 $cmd2}
-Write-Verbose $("lastTransferCount " + $SicherungStat.count) $(lastTransferSizeB " + $SicherungStat.sum)
+if (!$verbose) {.\fhemcl.ps1 $fhemurl $cmd1 $cmd2}
+Write-Verbose "$("lastTransferCount " + $SicherungStat.count) $("lastTransferSizeB " + $SicherungStat.sum)"
  
