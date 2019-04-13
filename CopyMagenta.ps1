c1426b3ca45dbc1c01b9d24d343a235e0a9f8e31
Otto <heinz-otto.klas@gmx.de>
2019-04-13 14:21:05 +0200

    Update CopyMagenta.ps1

--- CopyMagenta.ps1
+++ CopyMagenta.ps1
@@ -9,33 +9,42 @@
     This Script needs two external Scripts
     .\fhemcl.ps1
 #>
+#region Params
+param(
+[Parameter(Mandatory = $false, ValueFromPipeline = $true)]
+[string]$fhemurl,
+[string]$sourcepath,
+[string]$destination
+)
+#endregion 
+Write-Verbose $sourcepath
+Write-Verbose $destination
 Set-Location $PSScriptRoot
 
+#exit
+
 $FileHash = "LeftSideHash$(get-date -format "yyyyMMdd-HHmmss").txt"
-$sourcepath = "D:\Transfer"
-$destination = "D:\Magenta"
-$fhemurl = "http://192.168.178.104:8083"
 
-.\fhemcl.ps1 $fhemurl "set Sicherung Copy_2_$destination" 
-
-If (-not (test-path $destination\Scripts)) { md $destination\Scripts}
-If (-not (test-path $destination\Sicherung)) { md $destination\Sicherung}
+if ($fhemurl -ne "leer") {.\fhemcl.ps1 $fhemurl "set Sicherung Copy_2_$destination"} else {Write-Verbose "set Sicherung Copy_2_$destination"}
+$path=$destination + "\Scripts"
+If (-not (test-path $path)) {New-Item -Path $path -ItemType Directory| Out-Null} 
+$path=$destination + "\Sicherung"
+If (-not (test-path $path)) {New-Item -Path $path -ItemType Directory| Out-Null}
 
 # get only files from a certain date
 # $sourcefiles = gci $sourcepath -File -Recurse |where {(get-date $_.CreationTime -f "dd/MM/yy") -eq "11.04.19"}
 $sourcefiles = gci $sourcepath -File -Recurse |where {(get-date $_.CreationTime -f "dd/MM/yy") -eq (get-date -f "dd/MM/yy")}
 $LeftSideHash = $sourcefiles | Get-FileHash | select @{Label="Path";Expression={$_.Path.Replace($sourcepath,"")}},Hash 
 $LeftSideHash |Export-Clixml $destination\Scripts\$FileHash
-.\fhemcl.ps1 $fhemurl "set Sicherung Hashfile_erzeugt"
-
+if ($fhemurl -ne "leer") {.\fhemcl.ps1 $fhemurl "set Sicherung Hashfile_erzeugt"}else {Write-Verbose "set Sicherung Hashfile_erzeugt"}
 
 # Dateien kopieren, da einzelne Files kopiert werden ist das mit den Pfaden etwas aufwendiger
 foreach ($file in $sourcefiles)
 {
     $newdir = $file.DirectoryName.Replace( $sourcepath, $($destination + "\Sicherung") )
-    If (-not (test-path $newdir)) { md $newdir}
+    If (-not (test-path $newdir)) { md $newdir| Out-Null}
     Copy-Item -Path $file.FullName -Destination $newdir
-    .\fhemcl.ps1 $fhemurl $("set Sicherung " + $file.fullname)
+    if ($fhemurl -ne "leer") {.\fhemcl.ps1 $fhemurl $("set Sicherung " + $file.fullname)}else {Write-Verbose $("set Sicherung " + $file.fullname)}
 }
 
 #Exportiere FileCollection nach XML
@@ -45,6 +54,6 @@ gci $destination\Sicherung -r | Export-Clixml $destination\Scripts\Sicherung.xml
 
 $SicherungStat = gci $destination\Sicherung -r | measure-object -Property length -sum
 
-.\fhemcl.ps1 $fhemurl $("setreading Sicherung lastTransferCount " + $SicherungStat.count)
-.\fhemcl.ps1 $fhemurl $("setreading Sicherung lastTransferSizeB " + $SicherungStat.sum)
+if ($fhemurl -ne "leer") {.\fhemcl.ps1 $fhemurl $("setreading Sicherung lastTransferCount " + $SicherungStat.count)}else {Write-Verbose $("setreading Sicherung lastTransferCount " + $SicherungStat.count)}
+if ($fhemurl -ne "leer") {.\fhemcl.ps1 $fhemurl $("setreading Sicherung lastTransferSizeB " + $SicherungStat.sum)}else {Write-Verbose $("setreading Sicherung lastTransferSizeB " + $SicherungStat.sum)}
  
