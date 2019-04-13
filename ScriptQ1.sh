27f8e05b58f330bca978bc98230dd9091effcd0e
Otto <heinz-otto.klas@gmx.de>
2019-04-13 18:48:17 +0200

    Update ScriptQ1.sh
    input Foldernames from Arguments. No configuration inside the Script

--- ScriptQ1.sh
+++ ScriptQ1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # needs fhemcl.sh in the same path
-# Variables
-qpath="/media/zadonixs1"
-dpath="/media/magenta"
+# read Foldernames from Arguments
+qpath=$1
+dpath=$2
 sdir=$(dirname $(realpath "$0"))
 # mount, sync and trigger
 mount $qpath
@@ -10,6 +10,7 @@ mount $dpath
 rsync -rut --inplace ${qpath}/S* ${dpath}
 # delete the sourcefiles
 rm -r ${qpath}/S*
+# cp -pRu "${qpath}/S*" "${dpath}"
 bash "${sdir}/fhemcl.sh" 8083 "set Sicherung CopyMagentaEnde" > /dev/null 2>&1
 umount $qpath
 umount $dpath
