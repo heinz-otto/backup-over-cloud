11e56833d341ea5f9a2dbbc1a77e9aeeb38162df
Otto <heinz-otto.klas@gmx.de>
2019-04-16 15:00:47 +0200

    Update ScriptA1.sh

--- ScriptA1.sh
+++ ScriptA1.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 # this script sync the cloud content to the archive server
-# needs fhemcl.sh in the same path
 # Read the foldernames from Arguments
 qpath=$1
 dpath=$2
+#LOG=$0.log
 LOG=/opt/fhem/Script.log
 # check if fhemcl exists
 file=fhemcl.sh
+{
 if [ ! -e $file ]
 then
-    echo "$file is missing"  >> $LOG 2>&1
-    wget https://raw.githubusercontent.com/heinz-otto/fhemcl/master/$file  >> $LOG 2>&1
-    chmod +x $file  >> $LOG 2>&1
+    echo "$file is missing"
+    wget https://raw.githubusercontent.com/heinz-otto/fhemcl/master/$file
+    chmod +x $file
 fi
 # mount, sync and trigger
-mount $qpath >> $LOG 2>&1
-mount $dpath >> $LOG 2>&1
+mount "$qpath"
+mount "$dpath"
 # S* will copy all files and folders with S, folders will be created in $dpath if not exist
 # use more rsync lines for different folders
-rsync -a --delete ${qpath}/S* ${dpath} >> $LOG 2>&1
+rsync -a --delete "${qpath}/S*" "${dpath}"
 # set Status in FHEM
-bash fhemcl.sh 8083 "set Sicherung SyncEnde" >> $LOG 2>&1
-umount $qpath >> $LOG 2>&1
-umount $dpath >> $LOG 2>&1
+bash fhemcl.sh 8083 "set Sicherung SyncEnde"
+umount "$qpath"
+umount "$dpath"
+} >> $LOG 2>&1
