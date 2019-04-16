a1f28f516076f9e62089d8d70729f79e9992ec28
Otto <heinz-otto.klas@gmx.de>
2019-04-16 14:40:03 +0200

    Update ScriptA1.sh

--- ScriptA1.sh
+++ ScriptA1.sh
@@ -4,9 +4,15 @@
 # Read the foldernames from Arguments
 qpath=$1
 dpath=$2
-# detect the path from this script
-sdir=$(dirname $(realpath "$0"))
 LOG=/opt/fhem/Script.log
+# check if fhemcl exists
+file=fhemcl.sh
+if [ ! -e $file ]
+then
+    echo "$file is missing"  >> $LOG 2>&1
+    wget https://raw.githubusercontent.com/heinz-otto/fhemcl/master/$file  >> $LOG 2>&1
+    chmod +x $file  >> $LOG 2>&1
+fi
 # mount, sync and trigger
 mount $qpath >> $LOG 2>&1
 mount $dpath >> $LOG 2>&1
@@ -14,6 +20,6 @@ mount $dpath >> $LOG 2>&1
 # use more rsync lines for different folders
 rsync -a --delete ${qpath}/S* ${dpath} >> $LOG 2>&1
 # set Status in FHEM
-bash "${sdir}/fhemcl.sh" 8083 "set Sicherung SyncEnde" >> $LOG 2>&1
+bash fhemcl.sh 8083 "set Sicherung SyncEnde" >> $LOG 2>&1
 umount $qpath >> $LOG 2>&1
 umount $dpath >> $LOG 2>&1
