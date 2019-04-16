40abfccfff6ebae53f6d5794e0e04a745193460d
Otto <heinz-otto.klas@gmx.de>
2019-04-16 14:51:25 +0200

    Update ScriptS1.sh
    Shellcheck Suggestion

--- ScriptS1.sh
+++ ScriptS1.sh
@@ -7,21 +7,23 @@ dpath=$2
 LOG=/opt/fhem/ScriptS1.log
 # check if fhemcl exists
 file=fhemcl.sh
+{
 if [ ! -e $file ]
 then
-    echo "$file is missing"  >> $LOG 2>&1
-    wget https://raw.githubusercontent.com/heinz-otto/fhemcl/master/$file  >> $LOG 2>&1
-    chmod +x $file  >> $LOG 2>&1
+    echo "$file is missing"
+    wget https://raw.githubusercontent.com/heinz-otto/fhemcl/master/$file
+    chmod +x $file
 fi
 # mount, sync and trigger
-mount $qpath >> $LOG 2>&1
-mount $dpath >> $LOG 2>&1
-rsync -rut --inplace ${qpath}/S* ${dpath} >> $LOG 2>&1
-# delete the sourcefiles
-if [ $? -eq 0 ]; then
-   rm -r ${qpath}/S* >> $LOG 2>&1
+mount "$qpath"
+mount "$dpath"
+# if rsync ist ok then remove the synced files at source
+if ! rsync -rut --inplace "${qpath}/S*" "${dpath}"
+then
+   rm -r "${qpath}/S*"
 fi
 # cp -pRu "${qpath}/S*" "${dpath}"
-bash fhemcl.sh 8083 "set Sicherung CopyMagentaEnde" >> $LOG 2>&1
-umount $qpath >> $LOG 2>&1
-umount $dpath >> $LOG 2>&1
+bash fhemcl.sh 8083 "set Sicherung CopyMagentaEnde"
+umount "$qpath"
+umount "$dpath" 
+} >> $LOG 2>&1
