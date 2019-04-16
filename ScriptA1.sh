e416072e0ce285d93fe90e80074e21f54da8aee5
Otto <heinz-otto.klas@gmx.de>
2019-04-16 13:03:40 +0200

    Update ScriptA1.sh

--- ScriptA1.sh
+++ ScriptA1.sh
@@ -3,14 +3,17 @@
 # needs fhemcl.sh in the same path
 # Read the foldernames from Arguments
 qpath=$1
-dpath=$2 
+dpath=$2
 # detect the path from this script
-sdir=$(dirname $(realpath "$0")) 
+sdir=$(dirname $(realpath "$0"))
+LOG=/opt/fhem/Script.log
 # mount, sync and trigger
-mount $qpath
-mount $dpath
+mount $qpath >> $LOG 2>&1
+mount $dpath >> $LOG 2>&1
 # S* will copy all files and folders with S, folders will be created in $dpath if not exist
 # use more rsync lines for different folders
-rsync -a --delete ${qpath}/S* ${dpath}
+rsync -a --delete ${qpath}/S* ${dpath} >> $LOG 2>&1
 # set Status in FHEM
-bash "${sdir}/fhemcl.sh" 8083 "set Sicherung SyncEnde" > /dev/null 2>&1
+bash "${sdir}/fhemcl.sh" 8083 "set Sicherung SyncEnde" >> $LOG 2>&1
+umount $qpath >> $LOG 2>&1
+umount $dpath >> $LOG 2>&1
