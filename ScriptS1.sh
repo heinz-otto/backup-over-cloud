d2120953b61807cf11f95f917816c347b19f7d4b
Otto <heinz-otto.klas@gmx.de>
2019-04-16 14:38:17 +0200

    Update ScriptS1.sh

--- ScriptS1.sh
+++ ScriptS1.sh
@@ -3,9 +3,16 @@
 # read Foldernames from Arguments
 qpath=$1
 dpath=$2
-sdir=$(dirname $(realpath "$0"))
 #LOG=$0.log
 LOG=/opt/fhem/ScriptS1.log
+# check if fhemcl exists
+file=fhemcl.sh
+if [ ! -e $file ]
+then
+    echo "$file is missing"  >> $LOG 2>&1
+    wget https://raw.githubusercontent.com/heinz-otto/fhemcl/master/$file  >> $LOG 2>&1
+    chmod +x $file  >> $LOG 2>&1
+fi
 # mount, sync and trigger
 mount $qpath >> $LOG 2>&1
 mount $dpath >> $LOG 2>&1
@@ -15,6 +22,6 @@ if [ $? -eq 0 ]; then
    rm -r ${qpath}/S* >> $LOG 2>&1
 fi
 # cp -pRu "${qpath}/S*" "${dpath}"
-bash "${sdir}/fhemcl.sh" 8083 "set Sicherung CopyMagentaEnde" >> $LOG 2>&1
+bash "fhemcl.sh" 8083 "set Sicherung CopyMagentaEnde" >> $LOG 2>&1
 umount $qpath >> $LOG 2>&1
 umount $dpath >> $LOG 2>&1
