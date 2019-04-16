c298e6b75bbc610e0c909ef03d670bbf8a0309d7
Otto <heinz-otto.klas@gmx.de>
2019-04-16 12:41:33 +0200

    Update ScriptS1.sh

--- ScriptS1.sh
+++ ScriptS1.sh
@@ -4,16 +4,17 @@
 qpath=$1
 dpath=$2
 sdir=$(dirname $(realpath "$0"))
-LOG=$0.log
+#LOG=$0.log
+LOG=/opt/fhem/ScriptS1.log
 # mount, sync and trigger
-mount $qpath
-mount $dpath
+mount $qpath >> $LOG 2>&1
+mount $dpath >> $LOG 2>&1
 rsync -rut --inplace ${qpath}/S* ${dpath} >> $LOG 2>&1
 # delete the sourcefiles
 if [ $? -eq 0 ]; then
-   rm -r ${qpath}/S*
+   rm -r ${qpath}/S* >> $LOG 2>&1
 fi
 # cp -pRu "${qpath}/S*" "${dpath}"
-bash "${sdir}/fhemcl.sh" 8083 "set Sicherung CopyMagentaEnde" > /dev/null 2>&1
-umount $qpath
-umount $dpath
+bash "${sdir}/fhemcl.sh" 8083 "set Sicherung CopyMagentaEnde" >> $LOG 2>&1
+umount $qpath >> $LOG 2>&1
+umount $dpath >> $LOG 2>&1
