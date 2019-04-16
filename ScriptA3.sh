98a2328d50e89d110e5fbe7a6ecab5121e66ba90
Otto <heinz-otto.klas@gmx.de>
2019-04-16 15:08:13 +0200

    Update ScriptA3.sh
    ShellCheck Suggestions

--- ScriptA3.sh
+++ ScriptA3.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 # this script will sync the changes back to the cloud,
 # normally it will delete files wich are moved and deleted inside the powershell script
-# needs fhemcl.sh in the same path
+# needs fhemcl.sh in the same path should exists because of check in A1
 # read foldernames from Arguments
 qpath=$1
 dpath=$2
+#LOG=$0.log
 LOG=/opt/fhem/Script.log
-# check if fhemcl exists
-file=fhemcl.sh
-if [ ! -e $file ]
-then
-    echo "$file is missing"  >> $LOG 2>&1
-    wget https://raw.githubusercontent.com/heinz-otto/fhemcl/master/$file  >> $LOG 2>&1
-    chmod +x $file  >> $LOG 2>&1
-fi
 # sync, umount and trigger
-mount $qpath >> $LOG 2>&1
-mount $dpath >> $LOG 2>&1
+{
+mount "$qpath"
+mount "$dpath"
 # S* will copy all files and folders with S, folders will be created in $dpath if not exist
-# use more rsync lines for different folders
-rsync -a --delete ${qpath}/S* ${dpath} >> $LOG 2>&1
-umount $qpath >> $LOG 2>&1
-umount $dpath >> $LOG 2>&1
-# set Status in FHEM
-bash fhemcl.sh 8083 "set Sicherung beendet" >> $LOG 2>&1
+# use more rsync lines for different folders and set Status in FHEM
+if ! rsync -a --delete "${qpath}/S*" "${dpath}"
+then
+   bash fhemcl.sh 8083 "set Sicherung beendet"
+else
+   bash fhemcl.sh 8083 "set Sicherung ERROR_A3"
+fi
+umount "$qpath"
+umount "$dpath"
+} >> $LOG 2>&1
