45d0571bffa501f6eb9c187d29fcca73711dcb88
Otto <heinz-otto.klas@gmx.de>
2019-04-17 11:03:20 +0200

    Update ScriptA3.sh

--- ScriptA3.sh
+++ ScriptA3.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 # this script will sync the changes back to the cloud,
 # normally it will delete files wich are moved and deleted inside the powershell script
-# needs fhemcl.sh in the same path should exists because of check in A1
 # read foldernames from Arguments
 qpath=$1
 dpath=$2
-#LOG=$0.log
-LOG=/opt/fhem/Script.log
+LOG=$0.log
 # sync, umount and trigger
 {
 mount "$qpath"
 mount "$dpath"
 # S* will copy all files and folders with S, folders will be created in $dpath if not exist
 # use more rsync lines for different folders and set Status in FHEM
-if ! rsync -a --delete "${qpath}/S*" "${dpath}"
+if  rsync -a --delete ${qpath}/S* ${dpath}
 then
    bash fhemcl.sh 8083 "set Sicherung beendet"
 else
